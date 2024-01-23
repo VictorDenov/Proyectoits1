7dd3069aaceed7c94fb0296344e167f9893f2af5
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-23 14:53:23 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 
-$DB_HOST = $_ENV["localhost"];
+$DB_HOST = $_ENV["DB_HOST"];
 $DB_NAME = $_ENV["DB_NAME"];
 $DB_USER = $_ENV["DB_USER"];
 $DB_PASS = $_ENV["DB_PASS"];
 $DB_PORT = $_ENV["DB_PORT"];
 
- $con = mysqli_connect($DB_USER,$DB_PASS,$DB_NAME,$DB_PORT);
+ $con = mysqli_connect("$DB_USER","$DB_PASS","$DB_NAME","$DB_PORT");
 ?>
