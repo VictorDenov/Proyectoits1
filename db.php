979d4c3dde87ea3374faa99614abe85a0304a5b5
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-23 13:03:44 -0500

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,8 @@ $DB_USER = $_ENV["DB_USER"];
 $DB_PASS = $_ENV["DB_PASS"];
 $DB_PORT = $_ENV["DB_PORT"];
 
-$con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
+ $con = mysqli_connect($DB_USER,$DB_PASS,$DB_NAME,$DB_PORT);
+
 
 if ($con) {
     // La conexión fue exitosa, puedes realizar operaciones con la base de datos aquí.
