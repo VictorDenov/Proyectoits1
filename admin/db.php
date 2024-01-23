84059072673cfd274906258c750379ba7b60b005
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-23 13:04:23 -0500

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -1,13 +1,19 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $db = "restaurant";
 
-    $con = mysqli_connect($server,$user,$password,$db);
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASS = $_ENV["DB_PASS"];
+$DB_PORT = $_ENV["DB_PORT"];
 
-    if(!($con))
-    {
-        echo "Connection Error";
-    }
+ $con = mysqli_connect($DB_USER,$DB_PASS,$DB_NAME,$DB_PORT);
+
+
+if ($con) {
+    // La conexión fue exitosa, puedes realizar operaciones con la base de datos aquí.
+} else {
+    // Hubo un error en la conexión, puedes imprimir el mensaje de error.
+    echo mysqli_error($con);
+}
+
 ?>
