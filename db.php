4912ded47bb5d3d1f2ac7175e65da15c14dbf778
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-23 12:28:13 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-    
+
     $DB_HOST = $_ENV["DB_HOST"];
     $DB_NAME= $_ENV["DB_NAME"];
     $DB_PASS = $_ENV["DB_PASS"];
     $DB_PORT = $_ENV["DB_PORT"];
     $DB_DATABSE= $_ENV["DB_DATABSE"];
-    $con = mysqli_connect($DB_HOST,$DB_NAME,$DB_PASS, $DB_PORT,$DB_DATABSE);
+    $con = mysqli_connect($DB_HOST,$DB_NAME,$DB_PASS,$DB_PORT,$DB_DATABSE);
 
     if($con)
     {
