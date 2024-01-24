35fba54f1902396b1277f988043172c9745c9e05
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-24 00:14:20 -0500

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -1,19 +1,22 @@
 <?php
-
 $DB_HOST = $_ENV["DB_HOST"];
-$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
 $DB_USER = $_ENV["DB_USER"];
 $DB_PASS = $_ENV["DB_PASS"];
-$DB_PORT = $_ENV["DB_PORT"];
+$DB_NAME = $_ENV["DB_NAME"];
 
- $con = mysqli_connect($DB_USER,$DB_PASS,$DB_NAME,$DB_PORT);
+$conn = new mysqli($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME,$DB_PORT);
 
-
-if ($con) {
-    // La conexión fue exitosa, puedes realizar operaciones con la base de datos aquí.
-} else {
-    // Hubo un error en la conexión, puedes imprimir el mensaje de error.
-    echo mysqli_error($con);
+if ($conn->connect_error) {
 }
 
 ?>
+
+
+
+
+
+
+
+
+
