2293d73d98baf010a6a8d5b6caaa5d0f69ea7948
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-23 23:36:13 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,23 @@
 <?php
-$DB_HOST = $_ENV["RAILWAY_DATABASE_HOST"];
-$DB_PORT = $_ENV["RAILWAY_DATABASE_PORT"];
-$DB_USER = $_ENV["RAILWAY_USERNAME"];
-$DB_PASS = $_ENV["RAILWAY_PASSWORD"];
-$DB_NAME = $_ENV["RAILWAY_DATABASE"];
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_PORT = $_ENV["DB_PORT"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASS = $_ENV["DB_PASS"];
+$DB_NAME = $_ENV["DB_NAME"];
 
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
+$conn = new mysqli($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME,$DB_PORT);
 
 if ($conn->connect_error) {
-    die("Error de conexión a la base de datos: " . $conn->connect_error);
 }
-
-// Realizar operaciones en la base de datos aquí...
 
 $conn->close();
 ?>
+
+
+
+
+
+
+
+
+
