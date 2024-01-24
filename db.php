39edee3fa0a1a1fbf5be7f76b7d6d185fbaafb68
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-23 22:41:41 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,23 @@
 <?php
+$DB_HOST = $_ENV["MYSQLHOST"];
+$DB_PORT = $_ENV["MYSQLPORT"];
+$DB_USER = $_ENV["MYSQLUSER"];
+$DB_PASS = $_ENV["MYSQLPASSWORD"];
+$DB_NAME = $_ENV["MYSQL_DATABASE"];
 
-$DB_HOST = $_ENV["DB_HOST"];
-$DB_NAME = $_ENV["DB_NAME"];
-$DB_USER = $_ENV["DB_USER"];
-$DB_PASS = $_ENV["DB_PASS"];
-$DB_PORT = $_ENV["DB_PORT"];
-$URL = $_ENV["URL"];
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
 
- $con = mysqli_connect($DB_USER,$DB_PASS,$DB_NAME,$DB_PORT,$URL);
+$conn->close();
 ?>
+
+
+
+
+
+
+
