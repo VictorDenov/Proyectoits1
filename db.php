951148b169c8e352ee6fa4bc9da338c7f1e90ca1
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-23 22:47:59 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,12 @@
 <?php
-$DB_HOST = $_ENV["monorail.proxy.rlwy.net"];
-$DB_PORT = $_ENV["28758"];
-$DB_USER = $_ENV["root"];
-$DB_PASS = $_ENV["e15hcAEEHd14hEa233G3Dc5552F5a6FA"];
-$DB_NAME = $_ENV["railway"];
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_PORT = $_ENV["DB_PORT"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASS = $_ENV["DB_PASS"];
+$DB_NAME = $_ENV["DB_NAME"];
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Error de conexión a la base de datos: " . $conn->connect_error);
 }
