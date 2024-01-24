9d911f44b218bd8ed9cb86de7bd19a3f32ba00e9
VictorDenov <104399973+VictorDenov@users.noreply.github.com>
2024-01-24 11:00:55 -0500

    update

--- db.php
+++ db.php
@@ -1,25 +1,23 @@
 <?php
+// Configuración de la base de datos
+$host = "bl0wa2m5azpzbzvdcqvp-mysql.services.clever-cloud.com";
+$database = "bl0wa2m5azpzbzvdcqvp";
+$user = "umhwkiqvyol6lj5o";
+$port = 21682;
+$password = "yG72JY7HveBnCJsbQ46";
 
-$host="bl0wa2m5azpzbzvdcqvp-mysql.services.clever-cloud.com";
-$username="umhwkiqvyol6lj5o";
-$password="yG72JY7HveBnCJsbQ46a";
-$dbname="bl0wa2m5azpzbzvdcqvp";
-
-// Crea la conexión
-$conn = new mysqli($host,$username,$password,$dbname);
+// Intenta realizar la conexión
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 // Verifica la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+
 echo "Conexión exitosa";
-// ... Puedes realizar consultas y operaciones con la base de datos aquí
+
+// Ahora puedes realizar consultas u operaciones en la base de datos aquí
 
 // Cierra la conexión al finalizar
 $conn->close();
 ?>
-
-
-
-
-
